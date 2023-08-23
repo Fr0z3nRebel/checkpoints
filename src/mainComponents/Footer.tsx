57c4d2c3d1c9e428822a76a9d4788b8bce9eb1ad
John Adams <>
2023-08-22 23:25:30 -0500

    Add style to footer
     ### What
    - Add card style to footer
    
     ### Why
    - Did not add during previuos UI update
    - To be uniform with other elements
    
     ### Testing
    - Local

--- src/mainComponents/Footer.tsx
+++ src/mainComponents/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w3-padding w3-theme-d5">
+    <footer className="w3-card w3-padding w3-theme-d5">
       <p>Copyright &copy; 2023. Lefty Studios, LLC. All Rights Reserved.</p>
     </footer>
   );
